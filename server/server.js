2457c723331f84d135194219b56b245f1c520450
it21201714 <it21201714@my.sliit.lk>
2024-10-08 12:07:25 +0530

    implemented user controller and frontend

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require('cors');
 const bodyParser = require("body-parser");
 
 dotenv.config();
 
+
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const authRoutes = require("./routes/userRoute"); 
 const appointmRoute = require("./routes/appointmentRoute")
